ac55bdfe7c321c535e3186026f005ace4e7c217b
Alejandro <montalvocarrascoalejandro@gmail.com>
2023-01-25 10:13:52 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean aprobado = true;
+        int nota = 8;
+        double notaMedia = 8.9;
+        String felicitacion="Enhorabuena";
+        
+        
+        System.out.println(aprobado);
+        System.out.println(nota);
+        System.out.println(notaMedia);
+        System.out.println(felicitacion);
+        System.out.println("Hello world!");
+    }
+}
